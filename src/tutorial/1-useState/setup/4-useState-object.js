0ae2c16b65f58f2f7b3c63f8d152044ff83b7e1c
Enchan <enrico.gallus@gmail.com>
2021-11-02 05:49:47 +0900

    useState object and counters

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, {Fragment, useState} from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    const [person, setPerson] = useState({name: 'peter', age: 24, message: 'random message'})
+
+    const [name, setName] = useState('peter')
+    const [age, setAge] = useState(24)
+    const [message, setMessage] = useState('random message')
+
+    const changeMessage = () => {
+        //spread operator to copy original object and overwrite only message
+        //setPerson({...person, message: 'hello world'})
+        setMessage('Hello World')
+    }
+    return (
+        <Fragment>
+            <h3>{name}</h3>
+            <h3>{age}</h3>
+            <h3>{message}</h3>
+            <button className='btn' onClick={changeMessage}>Change Message</button>
+        </Fragment>);
 };
 
 export default UseStateObject;
