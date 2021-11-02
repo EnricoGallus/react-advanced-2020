c5fd3b43867ce4fb3787de1e469aa6256e01c7c3
Enchan <enrico.gallus@gmail.com>
2021-11-02 17:49:31 +0900

    useeffect basic

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+    const [value, setValue] = useState(0)
+    useEffect(() => {
+        console.log('runs each time value changes')
+        if (value >= 1) {
+            document.title = `New Messages(${value})`
+        }
+    },[value])
+
+    useEffect(() => {
+        console.log('runs only the first time the component is rendered')
+    }, [])
+
+    useEffect(() => {
+        console.log('runs each time the component is rendered')
+    })
+
+
+    console.log('render component')
+
+    return (
+        <>
+            <h1>{value}</h1>
+            <button className='btn' onClick={() => setValue(value + 1)}>Click Me!</button>
+        </>);
 };
 
 export default UseEffectBasics;
